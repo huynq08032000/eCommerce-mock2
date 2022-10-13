e60610f8dda2fbe644f608904de81b265817d689
huynq08032000 <55910030+huynq08032000@users.noreply.github.com>
2022-10-13 19:32:58 +0700

    commit lastest products

---
+++ src/Components/LastestProductsComponent/LastestProductsComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Title from "../TitleComponent/Title";
+
+const LastestProductComponent = () => {
+
+    return (
+        <>
+            <Title label={'Lastest Products'} />
+        </>
+    )
+}
+
+export default LastestProductComponent;
--- src/Pages/Homepage/HomePage.jsx
+++ src/Pages/Homepage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CarouselComponent from "../../Components/CarouselComponent/CarouselComponent";
 import HeaderComponent from "../../Components/HeaderComponent/HeaderComponent";
+import LastestProductComponent from "../../Components/LastestProductsComponent/LastestProductsComponent";
 import '../css/index.scss'
 const HomePage = () => {
 
@@ -9,6 +10,7 @@ const HomePage = () => {
             <HeaderComponent />
             <div className="main-container">
                 <CarouselComponent />
+                <LastestProductComponent/>
                 HomePage
             </div>
 
