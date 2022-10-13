cd1f0a997a82147635cca0c3588b6ee5599c2a0a
huynq08032000 <55910030+huynq08032000@users.noreply.github.com>
2022-10-13 17:19:59 +0700

    add css carousel

--- src/Components/CarouselComponent/CarouselComponent.jsx
+++ src/Components/CarouselComponent/CarouselComponent.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
+import '../CarouselComponent/css/index.scss'
 
-const CarouselComponent = () =>{
+const CarouselComponent = () => {
 
-    return(
+    return (
         <>
-            CarouselComponent
+            <Carousel showThumbs={false} infiniteLoop={true}>
+                <div className="carousel-items">this is slide 1</div>
+                <div className="carousel-items">this is slide 2</div>
+                <div className="carousel-items">this is slide 3</div>
+            </Carousel>
         </>
     )
 }
--- src/Pages/Homepage/HomePage.jsx
+++ src/Pages/Homepage/HomePage.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import CarouselComponent from "../../Components/CarouselComponent/CarouselComponent";
 import HeaderComponent from "../../Components/HeaderComponent/HeaderComponent";
-
+import '../css/index.scss'
 const HomePage = () => {
 
-    return(
+    return (
         <>
-            <HeaderComponent/>
-            HomePage
+            <HeaderComponent />
+            <div className="main-container">
+                <CarouselComponent />
+                HomePage
+            </div>
+
         </>
     )
 }
