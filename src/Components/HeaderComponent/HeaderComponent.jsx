3ef9bc52cb8d53c6bdb48202dc13f505c56a0bf4
huynq08032000 <55910030+huynq08032000@users.noreply.github.com>
2022-10-13 11:24:10 +0700

    add header component

---
+++ src/Components/HeaderComponent/HeaderComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeaderComponent = () => {
+
+    return(
+        <>
+            HeaderComponent
+        </>
+    )
+}
+
+export default HeaderComponent;
--- src/Pages/Homepage/HomePage.jsx
+++ src/Pages/Homepage/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import HeaderComponent from "../../Components/HeaderComponent/HeaderComponent";
 
 const HomePage = () => {
 
     return(
         <>
+            <HeaderComponent/>
             HomePage
         </>
     )
