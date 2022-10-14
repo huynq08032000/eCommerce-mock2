0095b44004bca7bfac79a81c188df49c108b1917
huynq08032000 <55910030+huynq08032000@users.noreply.github.com>
2022-10-14 16:06:30 +0700

    commit

--- src/Components/HeaderComponent/ChildComponent/SearchComponent/SearchComponent.jsx
+++ src/Components/HeaderComponent/ChildComponent/SearchComponent/SearchComponent.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import '../../css/index.scss'
 import 'antd/dist/antd.css';
-import { Button, Input, Space } from 'antd';
+import { Input, Space } from 'antd';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 
+const { Search } = Input;
 const SearhComponent = () => {
+    const onSearch = (value) => console.log(value);
 
     return (
-        <form style={{ display: 'flex' }}>
-            <Input type="text" placeholder="Search Products" style={{ padding: '7px 20px', margin: '0 10px', borderRadius: 0, minWidth: '200px' }} />
-            <Button type='primary' loading={false} className='search-btn'>Search</Button>
-        </form>
+        <Space direction="vertical" style={{
+            width: '100%',
+            borderRadius : '5px',
+            backgroundColor : '#C4C4C4'
+        }}>
+            <Search
+                addonBefore={<div style={{display : 'flex'}}><MenuOutlinedIcon/>Categories</div>}
+                placeholder="Search Items"
+                onSearch={onSearch}
+            />
+        </Space>
     )
 }
 
