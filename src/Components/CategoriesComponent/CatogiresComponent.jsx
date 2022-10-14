a54b04c49ae3ba93fed0f788bd4ca3dfe28b62ac
huynq08032000 <55910030+huynq08032000@users.noreply.github.com>
2022-10-14 17:19:53 +0700

    update categories

--- src/Components/CategoriesComponent/CatogiresComponent.jsx
+++ src/Components/CategoriesComponent/CatogiresComponent.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import 'antd/dist/antd.css';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import axios from "axios";
+import { getAllCategories } from "../../config/api";
+import CategoriesItem from "./CategoriesItems/CategoriesItem";
+import { modifyLetter } from "../../ultis/ultis";
 
 const CategoriesComponent = () => {
-
+    const [cateItems, setCateItems] = useState([])
+    useEffect(() => {
+        fetchCategories()
+    }, [])
+    const fetchCategories = async () => {
+        try {
+            const res = await axios.get(getAllCategories)
+            console.log(res.data.data)
+            setCateItems(res.data.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <>
-            <div style={{ display: 'flex' , alignItems :'center'}}><MenuOutlinedIcon /><span style={{fontSize : '20px', marginLeft : '10px'}}>Categories</span></div>
-            Categories
+            <div style={{ display: 'flex', alignItems: 'center', padding : '10px' }}><MenuOutlinedIcon /><span style={{ fontSize: '20px', marginLeft: '10px' }}>Categories</span></div>
+            {cateItems?.map((el, index)=>{
+                return <div key={index}>
+                    <CategoriesItem cate={modifyLetter(el)}/>
+                </div>
+            })}
         </>
     )
 }
