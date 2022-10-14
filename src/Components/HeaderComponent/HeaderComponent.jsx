67e48d701129bc982bcc27bb228b0ef78b8a8190
huynq08032000 <55910030+huynq08032000@users.noreply.github.com>
2022-10-14 15:57:20 +0700

    commit update header

--- src/Components/HeaderComponent/HeaderComponent.jsx
+++ src/Components/HeaderComponent/HeaderComponent.jsx
@@ -1,25 +1,50 @@
 import React from "react";
 import "../HeaderComponent/css/index.scss"
 import SearchComponent from '../HeaderComponent/ChildComponent/SearchComponent/SearchComponent'
-import PersonIcon from '@mui/icons-material/Person';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const HeaderComponent = () => {
-
+    const arrHeader = [
+        {
+            title : 'About us'
+        },
+        {
+            title : 'Contact'
+        },
+        {
+            title : 'Store',
+        },
+        {
+            title : 'Track Orders'
+        }
+    ]
     return (
         <>
+            <div className="header-container-temp">
+                {
+                    arrHeader.map((el, index) => {
+                        return(
+                            <div className="items" key={index}>
+                                {el.title}
+                            </div>
+                        )
+                    })
+                }
+            </div>
             <div className="header-container" >
                 <div className="left-side">
-                    <div className="title">Proshop</div>
-                    <div>
-                        <SearchComponent />
-                    </div>
+                    <div className="title">Shop app</div>
+                </div>
+                <div className="middle-side">
+                    <SearchComponent />
                 </div>
                 <div className="right-side">
-                    <div className="child-items"><ShoppingCartIcon fontSize="15px" style={{margin : '0 5px'}}/>Cart</div>
-                    <div className="child-items"><PersonIcon fontSize="15px" style={{margin : '0 5px'}}/>Signin</div>
+                    <div className="child-items"><ShoppingCartOutlinedIcon style={{ margin: '0 5px' }} /></div>
+                    <div className="child-items"><PersonOutlineOutlinedIcon style={{ margin: '0 5px' }} /></div>
                 </div>
             </div>
+
         </>
     )
 }
