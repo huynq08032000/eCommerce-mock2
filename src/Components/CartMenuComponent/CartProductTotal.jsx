3ca1240b55dc683c35fbd5fc3a37707604715e4c
huynq08032000 <55910030+huynq08032000@users.noreply.github.com>
2022-10-20 15:51:46 +0700

    commit cart page

--- src/Components/CartMenuComponent/CartProductTotal.jsx
+++ src/Components/CartMenuComponent/CartProductTotal.jsx
@@ -2,29 +2,25 @@ import { Button, Typography } from "@mui/material";
 import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { countSubtotal } from "../../ultis/ultis";
+import './css/index.scss'
 
-const style = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    margin: '10px 0px'
-}
-const CartProductTotal = () => {
+const CartProductTotal = ({ style }) => {
     const { cart } = useSelector(state => state.user)
     const subTotal = useMemo(() => countSubtotal(cart), [cart])
     const shippingPrice = 10.00
     return (
         <>
-            <div className="sub-total" style={style}>
-                <div><Typography fontWeight={700} fontSize={18}>Subtotal</Typography></div>
-                <div><Typography fontWeight={700} fontSize={18} color={'#727070'}>${subTotal}</Typography></div>
+            <div className="carttotal-item sub-total" style={style}>
+                <div><Typography fontWeight={700} fontSize={style === undefined ? 18 : style.fontSize}>Subtotal</Typography></div>
+                <div><Typography fontWeight={700} fontSize={style === undefined ? 18 : style.fontSize} color={'#727070'}>${subTotal}</Typography></div>
             </div>
-            <div className="shipping" style={style}>
-                <div><Typography fontWeight={700} fontSize={18}>Shipping</Typography></div>
-                <div><Typography fontWeight={700} fontSize={18} color={'#727070'}>${shippingPrice}</Typography></div>
+            <div className="carttotal-item shipping" style={style}>
+                <div><Typography fontWeight={700} fontSize={style === undefined ? 18 : style.fontSize}>Shipping</Typography></div>
+                <div><Typography fontWeight={700} fontSize={style === undefined ? 18 : style.fontSize} color={'#727070'}>${shippingPrice}</Typography></div>
             </div>
-            <div className="total" style={style}>
-                <div><Typography fontWeight={700} fontSize={18}>Total</Typography></div>
-                <div><Typography fontWeight={700} fontSize={18} color={'#727070'}>{subTotal + shippingPrice}</Typography></div>
+            <div className="carttotal-item total" style={style}>
+                <div><Typography fontWeight={700} fontSize={style === undefined ? 18 : style.fontSize}>Total</Typography></div>
+                <div><Typography fontWeight={700} fontSize={style === undefined ? 18 : style.fontSize} color={'#727070'}>{subTotal + shippingPrice}</Typography></div>
             </div>
         </>
     )
--- src/Pages/ShoppingCartPage/CartFooter.jsx
+++ src/Pages/ShoppingCartPage/CartFooter.jsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Button, TextField, Typography } from "@mui/material";
 import React from "react";
 import CartProductTotal from "../../Components/CartMenuComponent/CartProductTotal";
 
@@ -6,13 +6,15 @@ const CartFooter = () => {
 
     return (
         <>
-            <div className="cart-footer-containter" style={{ margin: '30px 0px' }}>
-                <div className="cart-footer-leftside">
-
+            <div className="cart-footer-containter" style={{ margin: '30px 0px', display: 'flex', justifyContent: 'space-between' }}>
+                <div className="cart-footer-leftside" style={{ display: 'flex', height: '56px' }}>
+                    <TextField id="outlined-basic" label="Coupon Code" variant="outlined" sx={{ backgroundColor: '#C4C4C4', width: '280px', marginRight: '10px' }} />
+                    <Button type="primary" sx={{ backgroundColor: '#FFD333' }}><Typography fontWeight={700} fontSize={18} color={'#000000'}>Apply Coupon</Typography></Button>
                 </div>
-                <div className="cart-footer-righttside" style={{ padding: '5px 10px 15px 10px', borderRadius: '5px', border: '1px solid rgba(90, 90, 90, 0.4)', width: '571px' }}>
+                <div className="cart-footer-righttside" style={{ padding: '20px 25px 20px 25px', borderRadius: '5px', border: '1px solid rgba(90, 90, 90, 0.4)', width: '571px', height: '450px' }}>
                     <Typography fontWeight={700} fontSize={34}>Cart Total</Typography>
-                    <CartProductTotal  />
+                    <CartProductTotal style={{ margin: '50px 0px', fontSize: '24px' }} />
+                    <Button type="primary" sx={{ backgroundColor: '#FFD333', width: '100%' }}><Typography fontWeight={700} fontSize={18} color={'#000000'}>Proceed to checkout</Typography></Button>
                 </div>
             </div>
         </>
